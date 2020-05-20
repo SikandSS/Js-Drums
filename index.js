db9a6d90e84a3738fdb9725acdce1d60616e151d
SikandSS <47498410+SikandSS@users.noreply.github.com>
2020-05-20 16:26:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,116 @@
+var buttons = document.getElementsByClassName('drum')
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function() {
+
+        var buttonInnerHTML = this.innerHTML;
+
+        buttonAnimation(buttonInnerHTML);
+
+        switch (buttonInnerHTML) {
+            case "w":
+                    var tom1 = new Audio("sounds/tom-1.mp3");
+                    tom1.play();
+                break;
+
+            case "a":
+                     var tom2 = new Audio("sounds/tom-2.mp3");
+                     tom2.play();
+                break;
+
+            case "s":
+                     var tom3 = new Audio("sounds/tom-3.mp3");
+                     tom3.play();
+                 break;
+
+            case "d":
+                     var tom4 = new Audio("sounds/tom-4.mp3");
+                      tom4.play();
+                 break;
+
+            case "j":
+                     var snare = new Audio("sounds/snare.mp3");
+                     snare.play();
+                 break;
+
+            case "k":
+                     var crash = new Audio("sounds/crash.mp3");
+                      crash.play();
+                 break;
+
+            case "l":
+                      var kick = new Audio("sounds/kick-bass.mp3");
+                      kick.play();
+                 break;
+        
+            default: console.log(buttonInnerHTML);
+                 break;
+                
+        }
+
+    })
+}   
+
+addEventListener("keydown", function(event) {
+
+    buttonAnimation(event.key);
+
+
+    switch (event.key) {
+        case "w":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+            break;
+
+        case "a":
+                 var tom2 = new Audio("sounds/tom-2.mp3");
+                 tom2.play();
+            break;
+
+        case "s":
+                 var tom3 = new Audio("sounds/tom-3.mp3");
+                 tom3.play();
+             break;
+
+        case "d":
+                 var tom4 = new Audio("sounds/tom-4.mp3");
+                  tom4.play();
+             break;
+
+        case "j":
+                 var snare = new Audio("sounds/snare.mp3");
+                 snare.play();
+             break;
+
+        case "k":
+                 var crash = new Audio("sounds/crash.mp3");
+                  crash.play();
+             break;
+
+        case "l":
+                  var kick = new Audio("sounds/kick-bass.mp3");
+                  kick.play();
+             break;
+    
+        default: console.log(buttonInnerHTML);
+             break;
+            
+    }
+    
+})
+
+
+
+// var audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
+
+
+
+function buttonAnimation(currentKey) {
+
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
